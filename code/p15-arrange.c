50c7f6ef27a5de10735fbb168bd8235e5d3b6a67
deepakrajendraprasad <deepak@iitpkd.ac.in>
2018-01-25 23:04:35 +0530

    Arragement

--- code/p15-arrange.c
+++ code/p15-arrange.c
@@ -7,49 +7,9 @@ int main()
 	printf("Please enter three integers: ");
 	scanf(" %d %d %d", &a, &b, &c);
 
-	if(a < b)
-	{
-		if (a < c)
-		{
-			printf("%d\n", a);
-			if (b < c)
-			{
-				printf("%d\n", b);
-				printf("%d\n", c);
-			}
-			else
-			{
-				printf("%d\n", c);
-				printf("%d\n", b);
-			}
-		}
-		else
-		{
-			printf("%d\n", c);
-			printf("%d\n", a);
-			printf("%d\n", b);
-		}
-	}
-	else if (b < c)
-	{
-		printf("%d\n", b);
-		if (a < c)
-		{
-			printf("%d\n", a);
-			printf("%d\n", c);
-		}
-		else
-		{
-			printf("%d\n", c);
-			printf("%d\n", a);
-		}
-	}
-	else
-	{
-		printf("%d\n", a);
-		printf("%d\n", b);
-		printf("%d\n", c);
-	}
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
 
 	return 0;
 }
